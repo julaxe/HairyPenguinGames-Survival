33497aa35163632f2b6c59b31c54650cc3ae410d
predator1245 <jashshah37@yahoo.com>
2022-02-05 20:38:02 +0100

    Add button click and death sound

--- HairyPenguin-SurvivalGame/Assets/[Scripts]/GameWorld/GameStateController.cs
+++ HairyPenguin-SurvivalGame/Assets/[Scripts]/GameWorld/GameStateController.cs
@@ -6,12 +6,14 @@ public class GameStateController : MonoBehaviour
 {
     [SerializeField] private Transform _currentSpawnPoint;
     private GameObject _player;
+    private AudioSource _deathAudio;
     private PlayerHealthController _playerHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Elf");
+        _deathAudio = GameObject.Find("GameOverSoundtrack").GetComponent<AudioSource>();
         _playerHealth = _player.GetComponent<PlayerHealthController>();
     }
 
@@ -28,6 +30,7 @@ public class GameStateController : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         _player.transform.position = _currentSpawnPoint.transform.position;
+        _deathAudio.Play();
     }
     public void updateCurrentSpawnPoint(Vector3 position)
     {
