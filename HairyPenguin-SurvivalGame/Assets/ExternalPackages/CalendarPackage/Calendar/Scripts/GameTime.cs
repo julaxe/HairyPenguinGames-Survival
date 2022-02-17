471e82b367afbbe62f03f1979904888c1e3d658e
Julaxe <julaxe8@gmail.com>
2022-02-17 14:00:22 -0500

    DayNight cycle, enemySpawner created

--- HairyPenguin-SurvivalGame/Assets/ExternalPackages/CalendarPackage/Calendar/Scripts/GameTime.cs
+++ HairyPenguin-SurvivalGame/Assets/ExternalPackages/CalendarPackage/Calendar/Scripts/GameTime.cs
@@ -8,5 +8,6 @@ public static class GameTime
    public static int Hours = 0;
    public static int TensMinutes = 0;
    public static int Minutes = 0;
+   public static bool Night = false;
 
 }
---
+++ HairyPenguin-SurvivalGame/Assets/[Scripts]/GameWorld/DayNightCycle.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using RenderSettings = UnityEngine.RenderSettings;
+
+public class DayNightCycle : MonoBehaviour
+{
+    private double _timer;
+    [Header("Time variables")]
+    [SerializeField] private int increaseRate = 30;
+    [SerializeField] private int tickRate = 1;
+
+    [Header("Skyboxes")]
+    [SerializeField] private Material daySkybox;
+    [SerializeField] private Material nightSkybox;
+    
+    [Header("directionalLight")]
+    public Light directionalLight;
+    [SerializeField] private float dayLightIntensity;
+    [SerializeField] private float nightLightIntensity;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTime();
+
+        if (GameTime.TensHours == 1 &&
+            GameTime.Hours == 8)
+        {
+            if (RenderSettings.skybox == nightSkybox) return;
+            RenderSettings.skybox = nightSkybox;
+            directionalLight.intensity = nightLightIntensity;
+            GameTime.Night = true;
+        }
+        else if (GameTime.TensHours == 0 &&
+                 GameTime.Hours == 6)
+        {
+            if (RenderSettings.skybox == daySkybox) return;
+            RenderSettings.skybox = daySkybox;
+            directionalLight.intensity = dayLightIntensity;
+            GameTime.Night = false;
+        }
+    }
+    
+    private void UpdateTime()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > tickRate)
+        {
+            GameTime.Minutes += increaseRate;
+            if (GameTime.Minutes >= 10)
+            {
+                GameTime.TensMinutes += GameTime.Minutes / 10;
+                GameTime.Minutes %= 10;
+                if (GameTime.TensMinutes >= 6)
+                {
+                    GameTime.Hours += GameTime.TensMinutes / 6;
+                    GameTime.TensMinutes %= 6;
+                    if (GameTime.TensHours >= 2 && GameTime.Hours >= 4)
+                    {
+                        GameTime.TensHours -= 2;
+                        GameTime.Hours -= 4;
+                        //GoToNextDay();
+                    }
+                    else if (GameTime.Hours >= 10)
+                    {
+                        GameTime.TensHours += GameTime.Hours / 10;
+                        GameTime.Hours %= 10;
+                    }
+                }
+
+            }
+            _timer = 0;
+            Debug.Log(GameTime.TensHours.ToString() + GameTime.Hours.ToString() + ":" + GameTime.TensMinutes.ToString() + GameTime.Minutes.ToString());
+        }
+    }
+}
